64edb8c7374365cc4dc18dc22454d7f45ba26f21
Ben Stone <benstone@uiowa.edu>
2022-06-03 10:59:10 -0500

    Add initial project files

---
+++ LearningEntityFramework/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace LearningEntityFramework;
+
+public class Program
+{
+    public static void Main()
+    {
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LearningEntityFramework
+
+This is a sandbox project for me to learn the C# Entity Framework.
